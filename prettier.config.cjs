2eb3a4cc54b425d6d9c59e89487f953682fd48d4
Lucas Farias <lukf95@gmail.com>
2023-08-20 12:40:49 +0200

    Update to use packlify for prettier

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,13 +1,3 @@
-module.exports = {
-  trailingComma: 'es5',
-  printWidth: 120,
-  arrowParens: 'avoid',
-  singleQuote: true,
-  importOrder: ['^@/(.*)$', '^[./]'],
-  importOrderSeparation: false,
-  importOrderSortSpecifiers: true,
-  importOrderGroupNamespaceSpecifiers: true,
-  importOrderCaseInsensitive: true,
-  plugins: ['@trivago/prettier-plugin-sort-imports', 'prettier-plugin-tailwindcss'],
-  pluginSearchDirs: false,
-};
+const config = require('@packlify/config-format/prettier.config.cjs');
+
+module.exports = { ...config };
