559e032e00648da94a4c78e1a364fb3ba1f9df4a
Lucas Farias <lukf95@gmail.com>
2023-09-23 14:23:40 +0200

    Save

--- src/server/entries/home.tsx
+++ src/server/entries/home.tsx
@@ -1,8 +1,6 @@
 import { renderComponent } from '../renderComponent';
 import Home from '@/shared/views/Home';
 
-// import { renderComponent } from './renderComponent';
-
 export function render(url: string, props: IInitialState) {
   return renderComponent({ Component: Home, url, props, withRouter: true });
 }
